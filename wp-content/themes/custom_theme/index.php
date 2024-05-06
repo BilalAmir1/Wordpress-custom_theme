17a4afd0dc0348ee8151d95d27071a48f13882a4
Bilal <bilalamir610@gmail.com>
2024-05-06 09:59:36 +0500

    first commit

---
+++ wp-content/themes/custom_theme/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package custom_theme
+ */
+
+ ?>
+
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress custom_theme</title>
+ </head>
+ <body>
+    
+ </body>
+ </html>
